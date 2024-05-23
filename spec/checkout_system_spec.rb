f258e9471cb187aa0205a16d3bf822e29cecd3c1
Ian Bayne <ianbayne@gmail.com>
2024-05-23 14:44:04 +0900

    test: add test for multiple promotions

--- spec/checkout_system_spec.rb
+++ spec/checkout_system_spec.rb
@@ -45,5 +45,31 @@ describe Checkout do
         expect(checkout.total_price).to eq 7500
       end
     end
+
+    context 'when there are multiple promotions' do
+      it 'provides a total for all scanned items minus all promotion discounts' do
+        checkout = Checkout.new
+        quantity_promotion_a = QuantityPromotion.new('A', 3, 7500)
+        quantity_promotion_b = QuantityPromotion.new('B', 2, 3500)
+        total_price_promotion = TotalPricePromotion.new(15000, 2000)
+        item_a = Item.new('A', 3000)
+        item_b = Item.new('B', 2000)
+        item_c = Item.new('C', 5000)
+        item_d = Item.new('D', 1500)
+
+        checkout.add_promotion(quantity_promotion_a)
+        checkout.add_promotion(quantity_promotion_b)
+        checkout.add_promotion(total_price_promotion)
+        checkout.scan(item_c)
+        checkout.scan(item_b)
+        checkout.scan(item_a)
+        checkout.scan(item_a)
+        checkout.scan(item_d)
+        checkout.scan(item_a)
+        checkout.scan(item_b)
+
+        expect(checkout.total_price).to eq 15500
+      end
+    end
   end
 end
